1fa01c3980efa48b05a19791d4adec678fb7b224
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2023-06-09 00:06:15 +0400

    Updated Save answers API call

--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -6,16 +6,11 @@ export async function callStoreAnswersAPI(answers) {
     const clientId = await getKeyPromise(CLIENT_ID_STORAGE_KEY);
     const clientEmail = await getKeyPromise(CLIENT_EMAIL_STORAGE_KEY);
 
-    const signature = getAwsSignature();
-
-    await fetch(process.env.REACT_APP_API_URL + '/save', {
+    await fetch(process.env.REACT_APP_API_URL + '/answers/', {
         method: 'POST',
-        mode: 'no-cors',
         body: JSON.stringify(buildStoreAnswerRequest(clientId, clientEmail, answers)),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
-            'Origin': '*',
-            ...signature.headers
         },
     }).then((response) => {
         return Promise.resolve()
@@ -29,29 +24,18 @@ function buildStoreAnswerRequest(clientId, clientEmail, answers) {
         "client_id": clientId,
         "client_email": clientEmail,
         "answer": {
-            "overall_productivity": answers.overall_productivity,
-            "productive_time_of_day": answers.productive_time_of_day,
-            "distractions_frequency": answers.distractions_frequency,
-            "worked_with_others_frequency": answers.worked_with_others_frequency,
-            "interruptions_frequency": answers.interruptions_frequency,
-            "amount_of_work_done": answers.amount_of_work_done,
-            "high_quality_work_done": answers.high_quality_work_done,
-            "breaks_frequency": answers.breaks_frequency,
-            "sleep_quality": answers.sleep_quality
+            "overall_productivity": answers.overall_productivity.toUpperCase(),
+            "productive_time_of_day": answers.productive_time_of_day.toUpperCase(),
+            "distractions_frequency": answers.distractions_frequency.toUpperCase(),
+            "worked_with_others_frequency": answers.worked_with_others_frequency.toUpperCase(),
+            "interruptions_frequency": answers.interruptions_frequency.toUpperCase(),
+            "amount_of_work_done": answers.amount_of_work_done.toUpperCase(),
+            "high_quality_work_done": answers.high_quality_work_done.toUpperCase(),
+            "breaks_frequency": answers.breaks_frequency.toUpperCase(),
+            "sleep_quality": answers.sleep_quality.toUpperCase()
         },
         "system_date": getCurrentSystemDate(),
         "system_time": getCurrentSystemTime(),
         "timezone": getSystemTimezone()
     }
-}
-
-function getAwsSignature() {
-    const aws4 = require('aws4');
-    const CREDS = { accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY, secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY }
-
-    return aws4.sign({
-        service: 'lambda',
-        region: 'eu-west-1',
-        host: process.env.REACT_APP_API_URL
-    }, CREDS)
 }

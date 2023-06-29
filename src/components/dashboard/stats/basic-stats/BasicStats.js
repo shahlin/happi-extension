b99d5203c819f58c7dcf777e839da0153207b3ca
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2023-06-29 15:10:50 +0400

    Added work quality chart

--- src/components/dashboard/stats/basic-stats/BasicStats.js
+++ src/components/dashboard/stats/basic-stats/BasicStats.js
@@ -1,11 +1,12 @@
 import './BasicStats.css'
 import SleepQualityStat from './sleep-quality/SleepQualityStat';
+import WorkQualityStat from './work-quality/WorkQualityStat';
 
 function BasicStats() {
     return (
         <div className="BasicStats">
             <SleepQualityStat />
-            <SleepQualityStat />
+            <WorkQualityStat />
         </div>
     );
 }
---
+++ src/components/dashboard/stats/basic-stats/work-quality/WorkQualityStat.js
@@ -0,0 +1,67 @@
+import { ResponsiveContainer, LineChart, Line } from "recharts";
+import BasicStatCard from "../BasicStatCard";
+import './WorkQualityStat.css'
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
+function WorkQualityStat() {
+    return (
+        <BasicStatCard>
+            <div className='DashboardStatHeader'>
+                <h2>Work Quality</h2>
+            </div>
+            <div className="BasicStatChart">
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart width={350} height={200} data={data}>
+                        <Line dataKey="pv" stroke="#D6D6D6" strokeWidth={2} dot={{ fill: '#6F6F6F', stroke: '#6F6F6F', strokeWidth: 2 }} />
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+        </BasicStatCard>
+    );
+}
+
+export default WorkQualityStat;

b97b41d8ffdecb88eb231b9ae11e046ca9ad6ce1
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2023-07-25 00:14:20 +0400

    [WIP] Daily stats UI updates

--- src/components/dashboard/stats/OverallYearlyStatCalendar.js
+++ src/components/dashboard/stats/OverallYearlyStatCalendar.js
@@ -38,7 +38,7 @@ function OverallYearlyStatCalendar(props) {
                 )
                 }
                 emptyColor="#D9D9D9"
-                colors={['#61cdbb', '#D3374E', '#FDC70E', '#2EAF6E']}
+                colors={['#61CDBB', '#D3374E', '#FDC70E', '#F1F1F1']}
                 margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
                 yearSpacing={40}
                 monthBorderColor="#ffffff"
@@ -63,10 +63,48 @@ function getRemappedDailySatisfactionData(data) {
         if (obj.value == "HAPPY") { obj.value = 3 }
     });
 
+    const todaysDate = new Date()
+    const endOfYearDate = new Date(getEndOfYear());
+    const upcomingDates = getDatesBetween(todaysDate, endOfYearDate)
+
+    upcomingDates.forEach(function (upcomingDate) {
+        data.daily.push({
+            day: getFormattedDate(upcomingDate),
+            value: 4
+        })
+    })
+
     return data.daily
 }
 
 function getStartOfYear() { return new Date().getFullYear() + "-01-01" }
 function getEndOfYear() { return new Date().getFullYear() + "-12-31" }
 
+function getDatesBetween(startDate, endDate) {
+    const currentDate = new Date(startDate.getTime());
+    const dates = [];
+    while (currentDate <= endDate) {
+        dates.push(new Date(currentDate));
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return dates;
+}
+
+function getFormattedDate(date) {
+    const year = date.toLocaleString('default', { year: 'numeric' });
+    const month = date.toLocaleString('default', { month: '2-digit' });
+    const day = date.toLocaleString('default', { day: '2-digit' });
+
+    return [year, month, day].join('-');
+}
+
 export default OverallYearlyStatCalendar;
+
+/*
+TODO:
+
+1. Upcoming dates shouldn't have tooltips
+2. Remove magic numbers and add constants for the values
+3. Fix green not showing (only 3 colors taken)
+*/

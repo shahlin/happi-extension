413d0a27d5f0b908acaff94cc2f0fafc8f0005f0
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2023-07-24 22:40:21 +0400

    Added weekly stats summary text

--- src/components/dashboard/stats/OverallWeeklyStat.js
+++ src/components/dashboard/stats/OverallWeeklyStat.js
@@ -1,20 +1,9 @@
 import { useEffect, useState } from 'react';
 import { getOverallSatisfactionStats } from '../../../api/stats/OverallSatisfactionStat';
 import './OverallWeeklyStat.css';
+import OverallWeeklyStatsSummary from './OverallWeeklyStatsSummary';
 
-async function getWeeklySatisfactionPercentages() {
-    var overallSatisfactionStat = await getOverallSatisfactionStats()
-
-    if (Object.keys(overallSatisfactionStat).length === 0) {
-        return {
-            "unhappy": 0,
-            "neutral": 0,
-            "happy": 0,
-        }
-    }
-
-    return overallSatisfactionStat.weekly
-}
+const PERCENTAGE_THRESHOLD_FOR_INSUFFICIENT_DATA_SUMMARY = 40
 
 function OverallWeeklyStat() {
     const [unhappyPercentage, setUnhappyPercentage] = useState(0);
@@ -35,7 +24,7 @@ function OverallWeeklyStat() {
 
     return (
         <div className='OverallWeeklyStat'>
-            <p className='OverallWeeklyStatText'>You have been quite unproductive the last week :c</p>
+            <p className='OverallWeeklyStatText'>{getWeeklySatisfactionSummary(unhappyPercentage, neutralPercentage, happyPercentage)}</p>
             <div className='OverallWeeklyStatBar'>
                 <div className="OverallWeeklyStatSingleBar" id='OverallWeeklyStatUnhappyBar' style={{ width: unhappyPercentage + "%", zIndex: 1 }}></div>
                 <div className="OverallWeeklyStatSingleBar" id='OverallWeeklyStatNeutralBar' style={{ width: neutralPercentage + "%", zIndex: 2 }}></div>
@@ -43,6 +32,48 @@ function OverallWeeklyStat() {
             </div >
         </div >
     );
+}
+
+async function getWeeklySatisfactionPercentages() {
+    var overallSatisfactionStat = await getOverallSatisfactionStats()
+
+    if (Object.keys(overallSatisfactionStat).length === 0) {
+        return {
+            "unhappy": 0,
+            "neutral": 0,
+            "happy": 0,
+        }
+    }
+
+    return overallSatisfactionStat.weekly
+}
+
+function getWeeklySatisfactionSummary(unhappyPercentage, neutralPercentage, happyPercentage) {
+    if ((unhappyPercentage + neutralPercentage + happyPercentage) < PERCENTAGE_THRESHOLD_FOR_INSUFFICIENT_DATA_SUMMARY) {
+        return OverallWeeklyStatsSummary.insufficient_data
+    }
+
+    if (unhappyPercentage > neutralPercentage && unhappyPercentage > happyPercentage) {
+        // Max Unhappy
+        return OverallWeeklyStatsSummary.unhappy
+    }
+
+    if (neutralPercentage > unhappyPercentage && neutralPercentage > happyPercentage) {
+        // Max Neutral
+        return OverallWeeklyStatsSummary.neutral
+    }
+
+    if (happyPercentage > unhappyPercentage && happyPercentage > neutralPercentage) {
+        // Max Happy
+        return OverallWeeklyStatsSummary.happy
+    }
+
+    if (unhappyPercentage == neutralPercentage && (unhappyPercentage + neutralPercentage) > happyPercentage) {
+        // Equally unhappy and neutral; Less happy
+        return OverallWeeklyStatsSummary.equal_data_unhappy_neutral
+    }
+
+    return OverallWeeklyStatsSummary.equal_data
 }
 
 export default OverallWeeklyStat;
---
+++ src/components/dashboard/stats/OverallWeeklyStatsSummary.js
@@ -0,0 +1,8 @@
+export default {
+    unhappy: "You have been quite unsatisfied this week, take it easy c:",
+    neutral: "The week's going okay so far, one at a time!",
+    happy: "Yay, looks like this week has been great so far!",
+    equal_data_unhappy_neutral: "Slow down, take enough breaks, breathe and focus, you got this!",
+    equal_data: "The week's been evenly balanced, keep calm and carry on! 🕺🏻",
+    insufficient_data: "Not enough inputs from you this week but hope you're being productive!",
+}

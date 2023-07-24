2a2ea4e63c6e26f6b54b2b76517a3c37b50b1a89
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2023-07-25 00:02:01 +0400

    Added daily stats integration

--- src/components/dashboard/stats/OverallYearlyStatCalendar.js
+++ src/components/dashboard/stats/OverallYearlyStatCalendar.js
@@ -1,33 +1,42 @@
 import { ResponsiveCalendar } from '@nivo/calendar'
 import './OverallYearlyStatCalendar.css';
+import { useEffect, useState } from 'react';
 
-function OverallYearlyStatCalendar() {
+function OverallYearlyStatCalendar(props) {
+
+    const [dailyData, setDailyData] = useState([])
+
+    async function handleFetchingStats() {
+        const data = await props.onFetchData();
+
+        setDailyData(getRemappedDailySatisfactionData(data))
+    }
+
+    useEffect(() => {
+        handleFetchingStats()
+    }, [dailyData])
+
     return (
         <div className='OverallYearlyStatCalendar'>
             <div className='DashboardStatHeader'>
                 <h2>Overall Satisfaction</h2>
             </div>
             <ResponsiveCalendar
-                data={[
-                    {
-                        "value": 1,
-                        "day": "2015-09-23"
-                    },
-                    {
-                        "value": 2,
-                        "day": "2015-07-31"
-                    },
-                    {
-                        "value": 3,
-                        "day": "2015-05-10"
-                    },
-                    {
-                        "value": 2,
-                        "day": "2015-12-16"
-                    }
-                ]}
-                from="2015-12-16"
-                to="2015-09-23"
+                data={dailyData}
+                from={getStartOfYear()}
+                to={getEndOfYear()}
+                tooltip={({ day, value, color }) => (
+                    <div style={{
+                        backgroundColor: 'rgba(100, 100, 100, 0.8)',
+                        padding: '6px 10px',
+                        fontSize: '16px',
+                        borderRadius: '7px',
+                        color: 'white'
+                    }}>
+                        {day}
+                    </div>
+                )
+                }
                 emptyColor="#D9D9D9"
                 colors={['#61cdbb', '#D3374E', '#FDC70E', '#2EAF6E']}
                 margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
@@ -36,8 +45,28 @@ function OverallYearlyStatCalendar() {
                 dayBorderWidth={3}
                 dayBorderColor="#ffffff"
             />
-        </div>
+        </div >
     );
 }
+
+function getRemappedDailySatisfactionData(data) {
+    if (data == null || data.length == 0 || Object.keys(data).length === 0) {
+        return []
+    }
+
+    data.daily.forEach(function (obj) {
+        obj.day = obj.date;
+        delete obj.date;
+
+        if (obj.value == "UNHAPPY") { obj.value = 1 }
+        if (obj.value == "NEUTRAL") { obj.value = 2 }
+        if (obj.value == "HAPPY") { obj.value = 3 }
+    });
+
+    return data.daily
+}
+
+function getStartOfYear() { return new Date().getFullYear() + "-01-01" }
+function getEndOfYear() { return new Date().getFullYear() + "-12-31" }
 
 export default OverallYearlyStatCalendar;

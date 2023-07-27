60de717e05b94509d1b2adf966d83b991d23899e
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2023-07-27 18:48:43 +0400

    Styled FAQ page

--- src/components/faq/PageContent.js
+++ src/components/faq/PageContent.js
@@ -1,10 +1,17 @@
 import { AccordionDetails, AccordionSummary, Typography } from '@mui/material';
-import './PageContent.css'
 import Accordion from '@mui/material/Accordion';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { QuestionAnswerList } from './QuestionAnswersList';
+import './PageContent.css'
+import { useState } from 'react';
 
 function PageContent() {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
         <div className="FaqPageContent">
             <h1>Frequently Asked Questions</h1>
@@ -13,16 +20,33 @@ function PageContent() {
             <div className='FaqDataContainer'>
                 {
                     QuestionAnswerList.map((entry, index) => {
-                        return (<Accordion>
+                        return (<Accordion
+                            elevation={0}
+                            disableGutters={true}
+                            className='FaqDataAccordion'
+                            expanded={expanded === ('panel' + index)}
+                            onChange={handleChange(('panel' + index))}
+                            sx={{
+                                '& .MuiTypography-root': { fontFamily: 'Dongle' },
+                                '.MuiAccordionSummary-root, .MuiAccordionDetails-root': { padding: 0 },
+                                '&:before': { display: 'none', } // To remove the bottom border
+                            }}>
                             <AccordionSummary
                                 expandIcon={<ExpandMoreIcon />}
                                 aria-controls={"panel" + (index + 1) + "a-content"}
                                 id={"panel" + (index + 1) + "a-header"}
                             >
-                                <Typography>{entry.question}</Typography>
+                                <Typography sx={{
+                                    fontSize: '32px',
+                                    // fontWeight: 'lighter',
+                                }}>{entry.question}</Typography>
                             </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>
+                            <AccordionDetails sx={{ marginBottom: '15px' }}>
+                                <Typography sx={{
+                                    fontSize: '22px',
+                                    fontWeight: 'lighter',
+                                    lineHeight: 1.2,
+                                }}>
                                     {entry.answer}
                                 </Typography>
                             </AccordionDetails>

30ce513c25ccd18f44c19cdda057646044403d4a
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2023-07-27 21:24:56 +0400

    Updated metrics information

--- src/components/metrics/MetricsList.js
+++ src/components/metrics/MetricsList.js
@@ -1,38 +1,38 @@
 export const MetricsList = [
     {
         question: "How'd you do today?",
-        answer: "When you find yourself coming close to the end of work day or later part of the day in general. Ideally, it's around the evening time of the day since most of us finish our work hours then. If you're someone who works at night, you might want to enter the data at night."
+        answer: "Simple, yet an important question to record how you feel. While we keep track of all the numbers and figures, we must also remember this qualitative piece of information, i.e, our emotion/mood. This metric helps us track the Satisfaction and Well-being dimension to understand how we feel across the day/week/month."
     },
     {
         question: "What time of the day were you most productive?",
-        answer: "The productivity data resets every night at 12:00 AM local time. If you continue your tasks/work beyond this time, your data should still be recorded for how you did the entire day before 12:00 AM. The work done after that should be considered as next day's work and the data can be entered the following day."
+        answer: "The goal of this metric is to find out at what time of the day we find ourselves being the most productive. This will help you understand when to have your focus time in a day. This is when you're in the flow state and highly productive."
     },
     {
         question: "Were you distracted?",
-        answer: "The answers are not publicly shared nor are they visible to any other users. They are only meant to be shown on the device and the Chrome user account where you have installed the extension. It is also in our interest to increase the data security in the days to come."
+        answer: "Did you find yourself falling into different sources of distractions? Or were you away from it all? This metric simply tracks how often you are distracted. This could be through social media, constant notifications, personal matter, or anything that keeps you from completing your task."
     },
     {
         question: "Did you work with other people?",
-        answer: "Yes, you can. While this is possible, it would not be recommended to change your answers and save them multiple times in a day. Ideally, you should enter your answers end of your work day and not have to go back to it unless you feel it's necessary. Reason being, towards the end of your work day, you can reflect upon the work you have done for the day and answer the questions all at once."
+        answer: "How often did you connect with other colleagues? This could be for some help you're asking for, a formal chat, a meeting to discuss something, or anything that includes work and humans! This helps us identify and track the Communication/Collaboration dimension better."
     },
     {
         question: "Were your interrupted during work?",
-        answer: "To avoid changing answers over and over again in a short span of time, the save button gets disabled right after you save for some time. The current duration for this disabling is set to 10 minutes. However, this duration may vary depending on the usage."
+        answer: "Interruptions can be in many forms. It could be as simple as a call while you're hard at work, someone breaking your flow state or ironically, even work meetings."
     },
     {
         question: "Did you do a lot of work?",
-        answer: "Each question (or metric) is trying to measure some specific dimension that leads to you being productive (or not). The metrics can be used in conjunction with one another to give you an idea about your productivity. The charts and graphs on your dashboard are the result of measuring these different metrics. To understand each question in detail, please head over to the [Metrics](https://www.shahlin.com) page."
+        answer: "This metric tracks the Activity dimension wherein it simply tracks the amount of work you have done in a day. It doesn't talk about the quality here, it's simply the quantity, the amount of efforts you have put."
     },
     {
         question: "Did you do high quality work?",
-        answer: "Each question (or metric) is trying to measure some specific dimension that leads to you being productive (or not). The metrics can be used in conjunction with one another to give you an idea about your productivity. The charts and graphs on your dashboard are the result of measuring these different metrics. To understand each question in detail, please head over to the [Metrics](https://www.shahlin.com) page."
+        answer: "While the above metric tracks the Activity side of things, this metric tries to track the Performance dimension. It tracks the quality of our work. It helps us understand if our work is adding value or is of high quality. Doing a lot of work might not necessarily mean doing high quality work."
     },
     {
         question: "Did you take enough breaks?",
-        answer: "Each question (or metric) is trying to measure some specific dimension that leads to you being productive (or not). The metrics can be used in conjunction with one another to give you an idea about your productivity. The charts and graphs on your dashboard are the result of measuring these different metrics. To understand each question in detail, please head over to the [Metrics](https://www.shahlin.com) page."
+        answer: "Amidst all of the work, socializing, etc, we must also take time to slow down and take a second to breathe. This metric aims to record and track the frequency of the breaks we take in middle of our work. Too much or too less can both be equally unproductive."
     },
     {
         question: "How was your sleep last night?",
-        answer: "Each question (or metric) is trying to measure some specific dimension that leads to you being productive (or not). The metrics can be used in conjunction with one another to give you an idea about your productivity. The charts and graphs on your dashboard are the result of measuring these different metrics. To understand each question in detail, please head over to the [Metrics](https://www.shahlin.com) page."
+        answer: "Sleep can be a huge factor in our day to day productivity. Lack of sleep can leave us feeling less motivated, less productive and in general not at our best state. That being said, everyone's sleep schedules and cycles differ and each one has his/her own way of handling it. This metric helps us find correlations between our sleep quality and our productivity."
     },
 ]

76585196049be0c0b53474b298101d2e0d8307cc
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2023-08-28 21:47:57 +0400

    Only notify if data is not added

---
+++ public/js/alarm.js
@@ -0,0 +1,48 @@
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+    const todaysDate = new Date();
+
+    // Notify at 4:30 every day
+    const notifyAt = new Date(
+        todaysDate.getFullYear(),
+        todaysDate.getMonth(),
+        todaysDate.getDate(),
+        16,
+        30,
+        0,
+        0
+    );
+
+    chrome.alarms.create('user-enter-data-alarm', {
+        periodInMinutes: 1440, // 24 hours in minutes
+        when: notifyAt.getMilliseconds()
+    });
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    if (alarm.name !== 'user-enter-data-alarm') {
+        return;
+    }
+
+    chrome.storage.sync.get('last_synced_at').then(function (value) {
+        const lastSyncedAtDate = new Date(value['last_synced_at']);
+        const todaysDate = new Date();
+
+        if (
+            lastSyncedAtDate.getFullYear() == todaysDate.getFullYear() &&
+            lastSyncedAtDate.getMonth() == todaysDate.getMonth() &&
+            lastSyncedAtDate.getDate() == todaysDate.getDate()
+        ) {
+            // Already added data
+        } else {
+            chrome.notifications.create(
+                `user-enter-data-notification-${Date.now()}`,
+                {
+                    type: 'basic',
+                    iconUrl: '../logo/logo-small-128.png',
+                    title: 'HAPPI',
+                    message: "Don't forget to fill out your inputs for today!",
+                }
+            )
+        }
+    });
+});
